3c5084ff236f3b115ab4ac43006debc25b67edb8
Lucas Lang <lucaslang2010@gmail.com>
2022-10-17 18:05:45 -0300

    Agregado opcion de modificar datos de un contacto

--- js/contacts.js
+++ js/contacts.js
@@ -1,13 +1,22 @@
 const d =document
 const $form = d.querySelector('form')
+let $listBtnsModi
+let ContactCode
+let EstoyModificando = false
+
 
 $form.addEventListener('submit',(e)=>{
     //e = evento, 
     e.preventDefault(); //cancela el comportamiento por defecto del evento
     console.log('Datos Capturados')
 
-    createContact($form.nombre.value, $form.correo.value, $form.telefono.value) //agregar parametros
-
+    if(EstoyModificando){
+        updateContact($form.nombre.value, $form.correo.value, $form.telefono.value,ContactCode)
+    }else{
+        createContact($form.nombre.value, $form.correo.value, $form.telefono.value) //agregar parametros
+    }
+    
+    EstoyModificando = false
     $form.reset()
 
     
@@ -15,6 +24,7 @@ $form.addEventListener('submit',(e)=>{
 
 
 readContacts()
+
 
 function readContacts() {
     let $template = ""
@@ -31,8 +41,8 @@ function readContacts() {
                                     <h4><b>${contacto.nombre}</b></h4>
                                     <p>${contacto.correo}</p>
                                     <p>${contacto.telefono}</p>
-                                    <button class="btn-card-delete"><i class="fa-sharp fa-solid fa-trash-can"></i></button>
-                                    <button class="btn-card-update"><i class="fa-sharp fa-solid fa-pen-to-square"></i></button>
+                                    <button class="btn-card-delete">Eliminar</button>
+                                    <button data-codigo='${contacto.id}' class="btn-card-update">Modificar</button>
                                 </div>
                             </div>`
                 
@@ -40,7 +50,34 @@ function readContacts() {
             });
             //ejecutando una unica vez. Fuera del for 
             document.querySelector('.list-contacts').innerHTML = $template
+
+            //obtenemos del html los botones de modificar
+            $listBtnsModi = d.querySelectorAll('.btn-card-update')
+
+            //recorrer array botones y por cada boton agrego una escucha al evento click, funcion anonima tipo flecha
+            //obteniendo id de usuario en consola
+            $listBtnsModi.forEach(boton =>{
+                boton.addEventListener('click',(e)=>{
+                    let id = e.target.dataset.codigo
+                    //llamada a la funcion que leera el contacto a modificar de data.json
+                    readOneContact(id)
+                    EstoyModificando = true
+                })
+            })
         })
+}
+
+//traera los datos eleguidos a modificar
+function readOneContact(idContact){
+    fetch('http://localhost:3000/contactos/' + idContact)
+    .then(res => res.json())
+        .then(contacto => {
+            ContactCode = contacto.id
+            $form.nombre.value = contacto.nombre
+            $form.correo.value = contacto.correo
+            $form.telefono.value = contacto.telefono
+        })
+        
 }
 
 //crea contactos con fetch, usando el boton guardar se almacena la informacion
@@ -72,3 +109,34 @@ function createContact(pnombre, pcorreo, ptelefono) {
             readContacts()
         })
 }
+
+function updateContact(nombre, correo, telefono, id) {
+
+    //guardando parametros dentro de un objeto -> dandole formato json para almacenarlos en data.json
+    const dataContact = {
+        nombre: nombre,
+        correo: correo,
+        telefono: telefono,
+        id: id
+    }
+
+    const infoRequest = {
+        method: 'PUT',
+        body: JSON.stringify(dataContact),
+        headers: {
+            'Content-Type': 'application/json'
+
+        }
+    }
+
+    console.log(infoRequest.body)
+
+    fetch('http://localhost:3000/contactos/' + dataContact.id, infoRequest)
+        .then(res => res.json())
+        .then(res => {
+            alert(`El cambio de ${res.nombre} se modifico correctamente`)
+            console.log(res)
+            readContacts()
+        })
+}
+
